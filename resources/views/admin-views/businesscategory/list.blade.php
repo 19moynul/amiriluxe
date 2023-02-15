d3918c054c8b782a9b861de311f99ee18cbd00c2
19moynul <19moynul@gmail.com>
2023-02-15 07:19:37 +0600

    ---------

--- resources/views/admin-views/businesscategory/list.blade.php
+++ resources/views/admin-views/businesscategory/list.blade.php
@@ -43,8 +43,11 @@
     </div>
 </div>
 
-<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
- <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+<script
+  src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"
+  integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30="
+  crossorigin="anonymous"></script>
     {{-- <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.12/datatables.min.js"></script> --}}
 <script>
     $(function () {
